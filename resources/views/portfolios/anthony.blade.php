54cc1c203af5e966da0fbfd2ec041c3a8d4c3c89
Tramaine Jackson <tjackson@sicom.com>
2019-02-28 11:48:33 -0500

    Finished updating portfolio

--- resources/views/portfolios/anthony.blade.php
+++ resources/views/portfolios/anthony.blade.php
@@ -69,7 +69,7 @@
 		</nav>
 
 		<!-- Intro Section -->
-		<div id="" class="view" style="min-height: 100vh; background-image: url('https://mdbootstrap.com/img/Photos/Others/architecture.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+		<div id="" class="view" style="min-height: 100vh; background-image: url('/images/anthony_o2.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
 			<div class="mask rgba-white-strong d-flex justify-content-center align-items-center">
 				<div class="container">
 					<div class="row smooth-scroll">
@@ -248,19 +248,30 @@
 					<div class="col-md-12 mb-5">
 
 						<!-- Section description -->
-						<h3 class="text-center w-responsive mx-auto wow fadeIn my-5" style="visibility: visible; animation-name: fadeIn;">DIY.</h3>
+						<h3 class="text-center w-responsive mx-auto">DIY.</h3>
+						<h4 class="text-center w-responsive mx-auto my-5" >I love DIY projects. Just finished remodelling a bathroom from top to bottom.</h4>
 
-						<div class="" id="">
-							<p>Before</p>
-							<img src="/images/bathroom_before1.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
-							<img src="/images/bathroom_before2.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
+						<p>Before</p>
+						<div class="d-lg-flex align-items-center justify-content-lg-around" id="">
+							<div class="col-12 col-lg-6" id="">
+								<img src="/images/bathroom_before1.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
+							</div>
+							<div class="col-12 col-lg-6" id="">
+								<img src="/images/bathroom_before2.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
+							</div>
 						</div>
 
-						<div class="" id="">
-							<p>After</p>
-							<img src="/images/bathroom_after1.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
-							<img src="/images/bathroom_after2.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
-							<img src="/images/bathroom_after3.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
+						<p>After</p>
+						<div class="d-lg-flex align-items-lg-center justify-content-lg-around" id="">
+							<div class="col-12 col-lg-4" id="">
+								<img src="/images/bathroom_after1.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
+							</div>
+							<div class="col-12 col-lg-4" id="">
+								<img src="/images/bathroom_after2.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
+							</div>
+							<div class="col-12 col-lg-4" id="">
+								<img src="/images/bathroom_after3.jpg" class="img-fluid z-depth-2 mb-3" alt="Responsive image">
+							</div>
 						</div>
 					</div>
 					<!-- /.First column -->
@@ -275,141 +286,6 @@
 
 		</div>
 		<!-- /.Third container -->
-
-		<!-- Contact form -->
-		<div id="contact" class="container">
-
-			<div class="streak1">
-				<div class="flex-center">
-					<ul class="list-unstyled">
-						<li><h2 class="h2-responsive mt-5 wow fadeIn" style="visibility: visible; animation-name: fadeIn;">Want an outstanding project?</h2></li>
-						<li><h5 class="h5-responsive wow fadeIn" data-wow-delay="0.2s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.2s;">Just send me a message!</h5></li>
-					</ul>
-				</div>
-			</div>
-
-			<!-- Section: Contact v.2 -->
-			<section id="contact" class="section pb-5 wow fadeIn" data-wow-delay="0.3s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.3s;">
-
-				<!-- Section heading -->
-				<h2 class="section-heading h1 pt-4">Contact Me</h2>
-
-				<div class="row">
-
-					<!-- Grid column -->
-					<div class="col-md-8 col-xl-9">
-
-						{!! Form::open(['action' => ['HomeController@message'], 'method' => 'POST']) !!}
-
-							<!-- Grid row -->
-							<div class="row">
-
-								<!-- Grid column -->
-								<div class="col-md-6">
-									<div class="md-form">
-										<div class="md-form">
-											<input type="text" id="contact-name" class="form-control" name="message_name" value="{{ old('message_name') }}" {{ $errors->any() ? 'autofocus' : '' }} required>
-											<label for="contact-name" class="">Your name</label>
-										</div>
-									</div>
-
-									@if($errors->has('message_name'))
-										<div class="red-text">
-											<span class="">{{ $errors->first('message_name') }}</span>
-										</div>
-									@endif
-								</div>
-								<!-- Grid column -->
-
-								<!-- Grid column -->
-								<div class="col-md-6">
-									<div class="md-form">
-										<div class="md-form">
-											<input type="text" id="contact-email" class="form-control" name="message_email" value="{{ old('message_email') }}" required>
-											<label for="contact-email" class="">Your email</label>
-										</div>
-									</div>
-
-									@if($errors->has('message_email'))
-										<div class="red-text">
-											<span class="">{{ $errors->first('message_email') }}</span>
-										</div>
-									@endif
-								</div>
-								<!-- Grid column -->
-
-							</div>
-							<!-- Grid row -->
-
-							<!-- Grid row -->
-							<div class="row">
-								<div class="col-md-12">
-									<div class="md-form">
-										<input type="text" id="contact-Subject" class="form-control" name="message_subject" value="{{ old('message_subject') }}" required>
-										<label for="contact-Subject" class="">Subject</label>
-									</div>
-
-									@if($errors->has('message_subject'))
-										<div class="red-text">
-											<span class="">{{ $errors->first('message_subject') }}</span>
-										</div>
-									@endif
-								</div>
-							</div>
-							<!-- Grid row -->
-
-							<!-- Grid row -->
-							<div class="row mb-4">
-
-								<!-- Grid column -->
-								<div class="col-md-12">
-									<div class="md-form">
-										<textarea type="text" name="message_body" id="contact-message" class="md-textarea form-control" rows="3" required>{{ old('message_body') }}</textarea>
-										<label for="contact-message">Your message</label>
-									</div>
-
-									@if($errors->has('message_body'))
-										<div class="red-text">
-											<span class="">{{ $errors->first('message_body') }}</span>
-										</div>
-									@endif
-								</div>
-							</div>
-							<!-- Grid row -->
-
-							<div class="text-center text-md-left mb-4">
-								<button type="submit" class="btn btn-light-blue waves-effect waves-light">Send</button>
-							</div>
-
-						{!! Form::close() !!}
-
-					</div>
-					<!-- Grid column -->
-
-					<!-- Grid column -->
-					<div class="col-md-4 col-xl-3">
-						<ul class="contact-icons">
-							<li><i class="fa fa-map-marker fa-2x"></i>
-								<p>Philadelphia, PA 19140, USA</p>
-							</li>
-
-							<li><i class="fa fa-phone fa-2x"></i>
-								<p>+ 1 267 879 4089</p>
-							</li>
-
-							<li><i class="fa fa-envelope fa-2x"></i>
-								<p>atstmpllc@gmail.com</p>
-							</li>
-						</ul>
-					</div>
-					<!-- Grid column -->
-				</div>
-
-			</section>
-			<!-- Section: Contact v.2 -->
-
-		</div>
-		<!-- Contact form -->
 
 	</main>
 	<!--/Main layout-->
